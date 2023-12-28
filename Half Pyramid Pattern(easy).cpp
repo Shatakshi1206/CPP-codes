fedae928ef0f8f582196a76f4e3721c20bb13ca8
Shatakshi1206 <153349188+Shatakshi1206@users.noreply.github.com>
2023-12-28 19:31:30 +0530

    Create Half Pyramid Pattern(easy).cpp
    Write a program to print given pattern by taking input N.
    
    Input Format
    The first line contains an integer N.
    
    Constraints
    Output Format
    Display the pattern as result.
    
    Sample Input
    5
    Sample Output
    1 
    1 2 
    1 2 3 
    1 2 3 4 
    1 2 3 4 5

---
+++ Half Pyramid Pattern(easy).cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int tr , rownum;
+    cin>>tr;
+    
+    rownum=1;
+    while(rownum<=tr){
+        
+        int i=1;
+        while(i<=rownum){
+            cout<<i<<" ";
+            i++;
+        }
+    rownum=rownum+1;
+    cout<<endl;
+    }
+    
+    return 0;
+}
