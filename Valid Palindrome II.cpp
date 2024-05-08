515ed1b5052f0b170b322acfad5986d5444585f1
Shatakshi1206 <153349188+Shatakshi1206@users.noreply.github.com>
2024-05-08 12:17:18 +0530

    Create Valid Palindrome II.cpp
    Given a string s, return true if the s can be palindrome after deleting at most one character from it.
    
     
    
    Example 1:
    
    Input: s = "aba"
    Output: true
    Example 2:
    
    Input: s = "abca"
    Output: true
    Explanation: You could delete the character 'c'.
    Example 3:
    
    Input: s = "abc"
    Output: false
     
    
    Constraints:
    
    1 <= s.length <= 105
    s consists of lowercase English letters.

---
+++ Valid Palindrome II.cpp
@@ -0,0 +1,27 @@
+class Solution {
+bool isPalindrome(string s,int i,int j) {
+    while(i<j){
+        if(s[i] != s[j]){
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
+}
+
+public:
+    bool validPalindrome(string s) {
+        int i=0;
+        int j=s.size()-1;
+        while(i<=j){
+            if(s[i] != s[j]){
+                return isPalindrome(s,i+1,j) || isPalindrome(s,i,j-1);
+            }
+            i++;
+            j--;
+        }
+    return true;
+    }
+};
+
